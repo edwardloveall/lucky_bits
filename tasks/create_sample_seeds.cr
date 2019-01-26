c22d532ab818569643777fa83d88d3fc4cb32ad2
Edward Loveall <edward@edwardloveall.com>
2019-01-26 16:43:39 -0500

    Create some sample data

--- tasks/create_sample_seeds.cr
+++ tasks/create_sample_seeds.cr
@@ -4,7 +4,49 @@ class Db::CreateSampleSeeds < LuckyCli::Task
   banner "Add sample database records helpful for development"
 
   def call
-    # generate sample data here
+    edward = SignUpForm.create!(
+      LuckyRecord::Params.new(
+        email: "edward@edwardloveall.com",
+        username: "edwardloveall",
+        password: "password",
+        password_confirmation: "password",
+      )
+    )
+    magic = SignUpForm.create!(
+      LuckyRecord::Params.new(
+        email: "magic@example.com",
+        username: "magic",
+        password: "password",
+        password_confirmation: "password",
+      )
+    )
+
+    BitForm.create!(
+      title: "KNOWER - 3",
+      url: "https://www.youtube.com/watch?v=5WwSNBJpB0Y",
+      user_id: edward.id,
+    )
+    BitForm.create!(
+      title: "Bad Kitty!",
+      url: "https://i.imgur.com/FbuLHsf.gifv",
+      user_id: edward.id,
+    )
+    BitForm.create!(
+      title: "Pixel Art Castle",
+      url: "https://www.deviantart.com/fool/art/iso-castle-75849130",
+      description: "This is some good inspiration",
+      user_id: magic.id,
+    )
+    BitForm.create!(
+      title: "Lobsters",
+      url: "https://lobste.rs",
+      description: "Here's that link site I was talking about",
+      user_id: magic.id,
+    )
+
+    params = LuckyRecord::Params.new(username: magic.username)
+    FollowRequestUsernameForm.create!(params, current_user: edward)
+
     puts "Done adding sample data"
   end
 end
