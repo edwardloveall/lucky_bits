e4d87600fe6da444a1df0346c32fed74f3eef03d
Edward Loveall <edward@edwardloveall.com>
2019-05-08 21:38:26 -0400

    Set chrome driver locally
    I've heard rumblings that the latest chromedriver was broken in some
    way. Luckily I had a local version installed that is not updated and
    not broken (2.44.609545). Setting the path explicitly to that in my
    test setup works great.

--- spec/setup/configure_lucky_flow.cr
+++ spec/setup/configure_lucky_flow.cr
@@ -1,5 +1,8 @@
 LuckyFlow.configure do |settings|
   settings.stop_retrying_after = 200.milliseconds
   settings.base_uri = Lucky::RouteHelper.settings.base_uri
+  {% if flag?(:darwin) %}
+    settings.chromedriver_path = "/usr/local/bin/chromedriver"
+  {% end %}
 end
 Spec.before_each { LuckyFlow::Server::INSTANCE.reset }
