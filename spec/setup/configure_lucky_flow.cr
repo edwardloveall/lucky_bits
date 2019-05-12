2987d4182f85ecb26b204568d2d62206e0442f32
Edward Loveall <edward@edwardloveall.com>
2019-05-11 20:57:15 -0400

    Include built in chromedriver binary

--- spec/setup/configure_lucky_flow.cr
+++ spec/setup/configure_lucky_flow.cr
@@ -1,8 +1,7 @@
 LuckyFlow.configure do |settings|
   settings.stop_retrying_after = 200.milliseconds
   settings.base_uri = Lucky::RouteHelper.settings.base_uri
-  {% if flag?(:darwin) %}
-    settings.chromedriver_path = "/usr/local/bin/chromedriver"
-  {% end %}
+  binary = "chromedriver-74-0-3729-6-mac64"
+  settings.chromedriver_path = "#{__DIR__}/../../vendor/#{binary}"
 end
 Spec.before_each { LuckyFlow::Server::INSTANCE.reset }
