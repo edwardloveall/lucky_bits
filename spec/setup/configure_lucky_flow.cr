e40898cd2b00f764cf70ce2ce5807daa54537f12
Edward Loveall <edward@edwardloveall.com>
2019-06-20 07:57:26 -0400

    Update travis config
    It seems that travis updated their platform to include xvfb, which
    means a large portion of the setup config isn't required. This probably
    happened sometime around April:
    
    https://github.com/travis-ci/docs-travis-ci-com/commit/52b61e97436b47192
    8080436c6a0b9b5f3da7409#diff-73ef4c05bd589b54026fb7ed7ef8fe24

--- spec/setup/configure_lucky_flow.cr
+++ spec/setup/configure_lucky_flow.cr
@@ -1,7 +1,5 @@
 LuckyFlow.configure do |settings|
   settings.stop_retrying_after = 200.milliseconds
   settings.base_uri = Lucky::RouteHelper.settings.base_uri
-  binary = "chromedriver-74-0-3729-6-mac64"
-  settings.chromedriver_path = "#{__DIR__}/../../vendor/#{binary}"
 end
 Spec.before_each { LuckyFlow::Server::INSTANCE.reset }
