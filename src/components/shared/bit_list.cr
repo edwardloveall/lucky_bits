9df08dacf94bcdc69bb0efa42c6689619a3b9c42
Edward Loveall <edward@edwardloveall.com>
2019-06-22 15:07:10 -0400

    Show bit description if it exists

--- src/components/shared/bit_list.cr
+++ src/components/shared/bit_list.cr
@@ -4,6 +4,9 @@ module Shared::BitList
       bits.each do |bit|
         li class: "bit" do
           link bit.title, to: bit.url, flow_id: "bit-title"
+          if description = bit.description
+            para description
+          end
           if @current_user == bit.user
             link "Edit", to: Bits::Edit.with(bit), flow_id: "edit-bit-#{bit.id}"
             link "Delete", to: Bits::Delete.with(bit), flow_id: "delete-bit-#{bit.id}"
