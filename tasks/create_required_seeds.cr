dc378c846dc983fe24d5cd8bf1902a271923ee00
Edward Loveall <edward@edwardloveall.com>
2019-09-08 18:19:01 -0400

    Update seeds

--- tasks/create_required_seeds.cr
+++ tasks/create_required_seeds.cr
@@ -4,7 +4,15 @@ class Db::CreateRequiredSeeds < LuckyCli::Task
   summary "Add database records required for the app to work"
 
   def call
-    # generate seeds here
+    edward = SignUpUser.create!(
+      Avram::Params.new(
+        email: "edward@edwardloveall.com",
+        username: "edwardloveall",
+        password: "change me",
+        password_confirmation: "change me",
+      )
+    )
+
     puts "Done adding required data"
   end
 end
--- tasks/create_sample_seeds.cr
+++ tasks/create_sample_seeds.cr
@@ -1,51 +1,49 @@
-require "../spec/support/boxes/**"
-
 class Db::CreateSampleSeeds < LuckyCli::Task
   summary "Add sample database records helpful for development"
 
   def call
-    edward = SignUpUser.create!(
+    edward = UserQuery.new.email("edward@edwardloveall.com").first
+    elizabeth = SignUpUser.create!(
       Avram::Params.new(
-        email: "edward@edwardloveall.com",
-        username: "edwardloveall",
+        email: "elizabeth@example.com",
+        username: "drliz",
         password: "password",
         password_confirmation: "password",
       )
     )
-    magic = SignUpUser.create!(
-      Avram::Params.new(
-        email: "magic@example.com",
-        username: "magic",
-        password: "password",
-        password_confirmation: "password",
-      )
-    )
+    fun_pod = SaveGroup.create!(title: "Fun Pod")
+    wifey_n_me = SaveGroup.create!(title: "Wifey 'n' Me")
+    SaveMembership.create!(user_id: edward.id, group_id: fun_pod.id)
+    SaveMembership.create!(user_id: elizabeth.id, group_id: fun_pod.id)
+    SaveMembership.create!(user_id: edward.id, group_id: wifey_n_me.id)
+    SaveMembership.create!(user_id: elizabeth.id, group_id: wifey_n_me.id)
 
     SaveBit.create!(
       title: "KNOWER - 3",
       url: "https://www.youtube.com/watch?v=5WwSNBJpB0Y",
       user_id: edward.id,
+      group_id: fun_pod.id,
     )
     SaveBit.create!(
       title: "Bad Kitty!",
       url: "https://i.imgur.com/FbuLHsf.gifv",
       user_id: edward.id,
+      group_id: wifey_n_me.id,
     )
     SaveBit.create!(
       title: "Pixel Art Castle",
       url: "https://www.deviantart.com/fool/art/iso-castle-75849130",
       description: "This is some good inspiration",
-      user_id: magic.id,
+      user_id: elizabeth.id,
+      group_id: wifey_n_me.id,
     )
     SaveBit.create!(
       title: "Lobsters",
       url: "https://lobste.rs",
       description: "Here's that link site I was talking about",
-      user_id: magic.id,
+      user_id: elizabeth.id,
+      group_id: fun_pod.id,
     )
-
-    params = Avram::Params.new(username: magic.username)
-    FollowRequestUsername.create!(params, current_user: edward)
 
     puts "Done adding sample data"
   end
