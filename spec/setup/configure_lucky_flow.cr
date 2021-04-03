17ae1a149b80ec0bfb43a6002c4e3a5190769405
Edward Loveall <edward@edwardloveall.com>
2021-04-03 16:03:42 -0400

    Try adding github CI

--- spec/setup/configure_lucky_flow.cr
+++ spec/setup/configure_lucky_flow.cr
@@ -1,11 +1,5 @@
 LuckyFlow.configure do |settings|
   settings.stop_retrying_after = 200.milliseconds
   settings.base_uri = Lucky::RouteHelper.settings.base_uri
-  {% if flag?(:darwin) %}
-    settings.driver_path = "vendor/chromedriver-89-mac64"
-  {% elsif flag?(:linux) %}
-    settings.driver_path = "vendor/chromedriver-89-linux64"
-  {% end %}
-  settings.screenshot_directory = "./tmp/screenshots"
 end
 Spec.before_each { LuckyFlow::Server::INSTANCE.reset }
