589a47746f83e12d71faa15739c411a93e7007fb
Alex Piechowski <alex@piechowski.io>
2022-04-12 06:55:56 -0500

    Fix CI
    - Upgrade breeze to support current lucky version
    - Update config/database.cr to latest generated app config
    - Change CI to use the https://github.com/grepsedawk/horseshoe ci.yml
      workflow
    
    It was locking up on db create/migrations of the test db

--- config/database.cr
+++ config/database.cr
@@ -5,15 +5,25 @@ AppDatabase.configure do |settings|
     settings.credentials = Avram::Credentials.parse(ENV["DATABASE_URL"])
   else
     settings.credentials = Avram::Credentials.parse?(ENV["DATABASE_URL"]?) || Avram::Credentials.new(
-      hostname: ENV["DB_HOST"]? || "localhost",
       database: database_name,
+      hostname: ENV["DB_HOST"]? || "localhost",
       port: ENV["DB_PORT"]?.try(&.to_i) || 5432,
+      # Some common usernames are "postgres", "root", or your system username (run 'whoami')
+      username: ENV["DB_USERNAME"]? || "postgres",
+      # Some Postgres installations require no password. Use "" if that is the case.
+      password: ENV["DB_PASSWORD"]? || "postgres"
     )
   end
 end
 
 Avram.configure do |settings|
   settings.database_to_migrate = AppDatabase
+
+  # In production, allow lazy loading (N+1).
+  # In development and test, raise an error if you forget to preload associations
   settings.lazy_load_enabled = LuckyEnv.production?
-  settings.query_cache_enabled = !LuckyEnv.test?
+
+  # Always parse `Time` values with these specific formats.
+  # Used for both database values, and datetime input fields.
+  # settings.time_formats << "%F"
 end
