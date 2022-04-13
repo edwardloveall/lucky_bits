bcf826f90a8fef0ec82b8e255893948a7cb505bb
Edward Loveall <edward@edwardloveall.com>
2022-04-12 20:19:56 -0400

    Edward's nitpicky edits

--- config/database.cr
+++ config/database.cr
@@ -8,9 +8,7 @@ AppDatabase.configure do |settings|
       database: database_name,
       hostname: ENV["DB_HOST"]? || "localhost",
       port: ENV["DB_PORT"]?.try(&.to_i) || 5432,
-      # Some common usernames are "postgres", "root", or your system username (run 'whoami')
       username: ENV["DB_USERNAME"]? || "postgres",
-      # Some Postgres installations require no password. Use "" if that is the case.
       password: ENV["DB_PASSWORD"]? || "postgres"
     )
   end
@@ -18,12 +16,5 @@ end
 
 Avram.configure do |settings|
   settings.database_to_migrate = AppDatabase
-
-  # In production, allow lazy loading (N+1).
-  # In development and test, raise an error if you forget to preload associations
   settings.lazy_load_enabled = LuckyEnv.production?
-
-  # Always parse `Time` values with these specific formats.
-  # Used for both database values, and datetime input fields.
-  # settings.time_formats << "%F"
 end
